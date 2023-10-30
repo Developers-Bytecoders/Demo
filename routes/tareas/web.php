f4437b28ecb58adf80689c8dc6117f660275e426
Cristopher Soobin Costales <105178704+CristopherCostales@users.noreply.github.com>
2023-10-30 16:17:22 -0600

    fix: delete documentation in web that doesnt use

--- routes/tareas/web.php
+++ routes/tareas/web.php
@@ -9,20 +9,8 @@ Route::get('tareas/create', [TareasController::class, 'create'])->name('tareas.c
 
 Route::post('tareas/create', [TareasController::class, 'store'])->name('tareas.store');
 
-
-/**
- * GET /{locale}/notifications/edit/{id}
- * @param locale string required lang answers. Example: "es" / "en"
- * @param notificationId int required notification slug. Example: "1"
- * Retrieves edit notification form.
- */
 Route::get('tareas/edit/{id}', [TareasController::class, 'edit'])->name('tareas.edit');
 
 Route::put('tareas/update/{tareaId}', [TareasController::class, 'update'])->name('tareas.update');
-/**
- * DELETE /{locale}/notifications/delete/{notificationId}
- * Delete an notification on notifications table.
- * @param locale string required lang answers. Example: "es" / "en"
- * @param notificationId string required notification id. Example: "1"
- */
+
 Route::delete('tareas/delete/{tareaId}', [TareasController::class, 'destroy'])->name('tareas.delete');
